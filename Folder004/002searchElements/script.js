435a21911e43b3eecac9930c0a97d6e06a5b0195
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-10 03:57:00 +0100

    En proceso

---
+++ Folder004/002searchElements/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona el enlace
+    const enlace = document.querySelector('a');
+
+    // Verifica si tiene el atributo target
+    if (enlace.hasAttribute("target")) {
+        // Imprime el valor del atributo target en la consola
+        console.log("Valor del atributo target:", enlace.getAttribute("target"));
+    } else {
+        // Imprime un mensaje si el atributo target no está presente
+        console.log("El atributo target no está presente");
+    }
+});
+
+/*
+ No tiene atributo "target" así que mostrará el mensaje del else.
+*/
